0f2b532381242eb1d8a11563817e8ff6e39bf01a
Dina O'Connor <DinaOC@Dinas-MacBook-Pro.local>
2016-02-16 09:04:30 -0500

    initial commit

---
+++ time-to-read.js
@@ -0,0 +1,22 @@
+
+
+var body= document.getElementsByTagName("p");
+var bodyString =" "
+  for (var i=0; i<body.length; i++){
+    bodyString +=body[i].innerHTML;
+}
+
+
+
+var wordsArray = bodyString.split(" ");
+console.log(wordsArray.length);
+var timeToRead =function(){
+  var min =0;
+  for ( var i=0; i<wordsArray.length; i+=230){
+    min = min+=1;
+  }
+  console.log(min+"minutes");
+  return min + "minutes";
+}
+
+timeToRead();
